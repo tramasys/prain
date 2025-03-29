70048781f057bae5bde6fb308a2d50f5755af879
tramasys <29458406+tramasys@users.noreply.github.com>
2025-03-29 12:10:44 +0100

    Add explicit default params to the python serial interface

--- src/comms/uart.py
+++ src/comms/uart.py
@@ -4,7 +4,16 @@ from prain_uart import Frame
 
 class UartInterface:
     def __init__(self, port: str, baudrate: int = 115200):
-        self.serial = serial.Serial(port, baudrate, timeout=1)
+        self.serial = serial.Serial(
+            port=port,
+            baudrate=baudrate,
+            bytesize=serial.EIGHTBITS,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            rtscts=False,
+            xonxoff=False,
+            timeout=1
+        )
 
     @property
     def port(self) -> str:
