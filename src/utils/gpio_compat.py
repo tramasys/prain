10012f0cef69393356b4967aff820c133820c131
tramasys <29458406+tramasys@users.noreply.github.com>
2025-05-16 16:35:05 +0200

    Add faked GPIO for local running

--- src/utils/gpio_compat.py
+++ src/utils/gpio_compat.py
@@ -6,20 +6,23 @@ otherwise a silent stub is provided so code instantiation doesn't fail.
 
 from types import SimpleNamespace
 
-try:
-    import RPi.GPIO as GPIO
-except (ImportError, RuntimeError):
-    def _noop(*_, **__): ...
-    class _FakePWM:
-        def __init__(self, pin, freq): self.pin, self.freq = pin, freq
-        start = stop = _noop
-        def ChangeFrequency(self, f): self.freq = f
-    GPIO = SimpleNamespace(
-        BOARD="BOARD", BCM="BCM",
-        OUT="OUT", IN="IN",
-        LOW=0, HIGH=1,
-        PWM=_FakePWM,
-        setmode=_noop, setup=_noop, output=_noop, cleanup=_noop,
-    )
+def _noop(*_, **__): ...
+class _FakePWM:
+    def __init__(self, pin, freq): self.pin, self.freq = pin, freq
+    start = stop = _noop
+    def ChangeFrequency(self, f): self.freq = f
 
-__all__ = ["GPIO"]
+GPIO = SimpleNamespace(
+    # modes
+    BOARD="BOARD", BCM="BCM",
+    # directions
+    OUT="OUT", IN="IN",
+    # logic levels
+    LOW=0, HIGH=1,
+    # resistor pulls
+    PUD_OFF=0, PUD_DOWN=1, PUD_UP=2,
+    # other API elements
+    PWM=_FakePWM,
+    setmode=_noop, setup=_noop, output=_noop, input=lambda *a, **k: 0,
+    cleanup=_noop, setwarnings=_noop,
+)
